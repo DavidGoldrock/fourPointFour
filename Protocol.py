b75a9a7f109b048017f8221183c0f343e4d490f0
David Goldrock <gaming131213@gmail.com>
2022-11-10 10:22:58 +0200

    first commit

---
+++ Protocol.py
@@ -0,0 +1,134 @@
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
+from io import TextIOWrapper
+
+FORMAT = 'utf-8'
+PORT = 80
+MAX_SIZE = 2048
+VERSION = 1.1
+responses = {
+	100: ('Continue', 'Request received, please continue'),
+	101: ('Switching Protocols',
+	      'Switching to new protocol; obey Upgrade header'),
+
+	200: ('OK', 'Request fulfilled, document follows'),
+	201: ('Created', 'Document created, URL follows'),
+	202: ('Accepted',
+	      'Request accepted, processing continues off-line'),
+	203: ('Non-Authoritative Information', 'Request fulfilled from cache'),
+	204: ('No Content', 'Request fulfilled, nothing follows'),
+	205: ('Reset Content', 'Clear input form for further input.'),
+	206: ('Partial Content', 'Partial content follows.'),
+
+	300: ('Multiple Choices',
+	      'Object has several resources -- see URI list'),
+	301: ('Moved Permanently', 'Object moved permanently -- see URI list'),
+	302: ('Found', 'Object moved temporarily -- see URI list'),
+	303: ('See Other', 'Object moved -- see Method and URL list'),
+	304: ('Not Modified',
+	      'Document has not changed since given time'),
+	305: ('Use Proxy',
+	      'You must use proxy specified in Location to access this '
+	      'resource.'),
+	307: ('Temporary Redirect',
+	      'Object moved temporarily -- see URI list'),
+
+	400: ('Bad Request',
+	      'Bad request syntax or unsupported method'),
+	401: ('Unauthorized',
+	      'No permission -- see authorization schemes'),
+	402: ('Payment Required',
+	      'No payment -- see charging schemes'),
+	403: ('Forbidden',
+	      'Request forbidden -- authorization will not help'),
+	404: ('Not Found', 'Nothing matches the given URI'),
+	405: ('Method Not Allowed',
+	      'Specified method is invalid for this server.'),
+	406: ('Not Acceptable', 'URI not available in preferred format.'),
+	407: ('Proxy Authentication Required', 'You must authenticate with '
+	                                       'this proxy before proceeding.'),
+	408: ('Request Timeout', 'Request timed out; try again later.'),
+	409: ('Conflict', 'Request conflict.'),
+	410: ('Gone',
+	      'URI no longer exists and has been permanently removed.'),
+	411: ('Length Required', 'Client must specify Content-Length.'),
+	412: ('Precondition Failed', 'Precondition in headers is false.'),
+	413: ('Request Entity Too Large', 'Entity is too large.'),
+	414: ('Request-URI Too Long', 'URI is too long.'),
+	415: ('Unsupported Media Type', 'Entity body in unsupported format.'),
+	416: ('Requested Range Not Satisfiable',
+	      'Cannot satisfy request range.'),
+	417: ('Expectation Failed',
+	      'Expect condition could not be satisfied.'),
+
+	500: ('Internal Server Error', 'Server got itself in trouble'),
+	501: ('Not Implemented',
+	      'Server does not support this operation'),
+	502: ('Bad Gateway', 'Invalid responses from another server/proxy.'),
+	503: ('Service Unavailable',
+	      'The server cannot process the request due to a high load'),
+	504: ('Gateway Timeout',
+	      'The gateway server did not receive a timely response'),
+	505: ('HTTP Version Not Supported', 'Cannot fulfill request.'),
+}
+
+httpOptions = ["GET", "POST"]
+
+filenameToType = {
+	"pdf": "application/pdf",
+	"json": "application/json",
+	"xml": "application/xml",
+	"zip": "application/zip",
+	"gif": "image/gif",
+	"jpeg": "image/jpeg",
+	"jpg": "image/jpeg",
+	"png": "image/png",
+	"js": "text/javascript; charset=UTF-8",
+	"css": "text/css",
+	"csv": "text/csv",
+	"html": "text/html",
+	"mp4": "video/mp4",
+	"ico": "image/vnd.microsoft.icon",
+}
+
+
+def startsWithOption(data: list | str, options: list[list] | list[str]) -> list | str:
+	for option in options:
+		try:
+			if data[:len(option)] == option:
+				return option
+		except IndexError:
+			pass
+	return None
+
+
+def formatHttp(code: int, headers: str = None, params: bytes = None) -> bytes:
+	data = params if params is not None else []
+	now = datetime.now()
+	stamp = mktime(now.timetuple())
+	response = f"HTTP/1.1 {code} {responses[code][0]}\r\nDate: {format_date_time(stamp)}\r\nContent-Length: {str(len(data))}\r\n".encode(
+		FORMAT)
+	if headers is not None:
+		response += headers.encode(FORMAT)
+	response += "\r\n".encode(FORMAT)
+	if params is not None:
+		response += params
+	return response
+
+
+def formatFileHttp(code: int, path: str, file: TextIOWrapper) -> bytes:
+	data = file.read()
+	return formatHttp(code,
+	                  f"Content-Type: {filenameToType[path[path.rfind('.') + 1:]]}\r\n", data)
+
+
+def calculateNext(data: dict) -> int:
+	return int(data["num"]) + 1
+
+
+def calculateArea(data: dict) -> int:
+	return int(float(data["width"])) * int(float(data["height"])) // 2
+
+
+functions = {"/calculate-next": lambda data: calculateNext(data), "/calculate-area": lambda data: calculateArea(data)}
---
+++ server.py
@@ -0,0 +1,145 @@
+import socket
+from threading import Thread
+import os
+import Protocol
+
+# green terminal:
+os.system('color a')
+# create socket (works out of main. if you make a server you want this to run always)
+DEBUGGING = True
+CLIENT_COUNT = 0
+SERVER = '127.0.0.1'
+ADDR = (SERVER, Protocol.PORT)
+server = socket.socket()
+
+
+# functions
+
+
+def setup():
+	global server
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind(ADDR)
+
+
+def ReceiveRequest(conn: socket) -> bool:
+	"""
+	:param conn: client sending message
+	:return: bytearray of data
+	"""
+	shouldStay = True
+	try:
+		data = conn.recv(Protocol.MAX_SIZE)
+		data, params = data.split(b"\r\n\r\n")
+		data = data.decode(Protocol.FORMAT)
+	# print("<<<<" + repr(data) + "<END>")
+	except socket.error | UnicodeDecodeError:
+		return False
+
+	if len(data) != 0:
+		response = handleRequest(data)
+		# print(">>>>>" + repr(response) + "<END>")
+		conn.sendall(response)
+	else:
+		shouldStay = False
+	return shouldStay
+
+
+def checkValidity(data: str) -> tuple[bool, int, str, list[str]]:
+	option = Protocol.startsWithOption(data, Protocol.httpOptions)
+	if option is None:
+		return False, 0, "", [""]
+	data = data.strip(option)
+	path = data[:data.index("HTTP/1.1")]
+	data = data[len(path):]
+	if data[:8] != "HTTP/1.1":
+		return False, 0, "", [""]
+	else:
+		data = data[8:]
+		if data[:1] != "\r\n":
+			decodedValue = data[2:].split("\r\n")
+			return True, option, path.strip(), decodedValue
+		else:
+			return False, 0, "", [""]
+
+
+def getFromFile(path) -> bytes:
+	if ".." in path:
+		response = Protocol.formatHttp(401)
+	else:
+		if path == "/":
+			path = "/index.html"
+		try:
+			with open("./webroot" + path, "rb") as file:
+				response = Protocol.formatFileHttp(200, path, file)
+		except OSError:
+			response = Protocol.formatHttp(500)
+	return response
+
+
+def handleRequest(data: str):
+	response = b""
+	valid, option, path, headers = checkValidity(data)
+	if not valid:
+		response = Protocol.formatHttp(400)
+	else:
+		if option == "GET":
+			function = Protocol.startsWithOption(path, Protocol.functions.keys())
+			if function is None:
+				response = getFromFile(path)
+			else:
+				responseFunc = Protocol.functions[function]
+				try:
+					if "?" not in path:
+						response = Protocol.formatHttp(200, None, str(responseFunc()).encode(Protocol.FORMAT))
+					else:
+						function, params_string = path.split("?")
+						# create a new dictionary x=2&y=3 -> {"x":"2","y":"3"}
+						params = {param.split("=")[0]: param.split("=")[1] for param in params_string.split("&")}
+						response = Protocol.formatHttp(200, None, str(responseFunc(params)).encode(Protocol.FORMAT))
+				except KeyError as e:
+					response = Protocol.formatHttp(400)
+		elif option == "POST":
+			print(path)
+			response = Protocol.formatHttp(500)
+	return response
+
+
+def handleClient(conn: socket):
+	global CLIENT_COUNT
+	"""
+	:param conn: the client socket
+	:return: None
+	"""
+	STAY = True
+	print(f"[RECEIVED REQUEST]")
+	while STAY:
+		STAY = ReceiveRequest(conn)
+		if not STAY:
+			CLIENT_COUNT -= 1
+			conn.close()
+			print(f"[FINISHED REQUEST]")
+	exit()
+
+
+def start():
+	global server
+	global CLIENT_COUNT
+	while True:
+		server.listen()
+		conn = server.accept()[0]
+		Thread(target=handleClient, args=(conn,), daemon=True).start()
+		CLIENT_COUNT += 1
+		print(f"[STATUS] Number of active users:{CLIENT_COUNT}")
+
+
+def debugPrinting():
+	pass
+
+
+if __name__ == '__main__':
+	setup()
+	if DEBUGGING:
+		debugPrinting()
+	print(f"[STARTING] SERVER: {SERVER} PORT: {Protocol.PORT}")
+	start()
